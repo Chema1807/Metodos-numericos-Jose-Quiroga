47d688ffef1ed2a832755ad79ccb40d27ecf4705
Chema1807 <chema1807@gmail.com>
2018-08-06 09:43:28 -0500

    Create <=0.py

---
+++ <=0.py
@@ -0,0 +1,8 @@
+"""
+Create a function that takes a number as its only argument and returns True if it's less than or equal to zero, otherwise return False.
+"""
+def lessThanOrEqualToZero(num):
+	if num<=0:
+		return True
+	else:
+		return False
